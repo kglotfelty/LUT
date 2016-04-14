106ceb50ba1a0390b2f7df2afa18e61430cb93b9
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2016-04-14 11:40:51 -0400

    color curves working with rgb tripples

--- color_curves.py
+++ color_curves.py
@@ -39,8 +39,6 @@ def _sample_from_lut( lutfile, num_sample, reverse=False, invert=False, xform=_l
     Load a color lookup table and sample num_sample many colors from it.
     
     """
-    from pycrates import read_file, get_colvals
-
     def _try_hard_to_locate( filename ):
         """
         Try to locate the LUT file just based on the name (eg returned
@@ -49,26 +47,56 @@ def _sample_from_lut( lutfile, num_sample, reverse=False, invert=False, xform=_l
         """
         import os as os
         import glob as glob
+        from pycrates import read_file, get_colvals
         
         tox = [ "", "{}/data/".format( os.environ["ASCDS_INSTALL"] ), 
                   "{}/data/".format(os.environ["ASCDS_CONTRIB"]),
                   "{}/.ds9/".format(os.environ["HOME"])]
 
         tox.extend(glob.glob( "{}/.ds9/LUT/*/".format( os.environ["HOME"]) ) )
-       
+
+        tab = None
         for tt in tox:
+            
             try:
-                return read_file( tt+filename )
+                tab = read_file( tt+filename )
+                break
             except:
                 pass
             
             try:
-                return read_file( tt+filename+".lut")
+                tab = read_file( tt+filename+".lut")
+                break
             except:
                 pass
-        
-        raise IOError("Could not find lookup table '{}'.  Maybe try full path?".format(filename ))
 
+        if tab is None:
+            raise IOError("Could not find lookup table '{}'.  Maybe try full path?".format(filename ))
+        
+        rr = get_colvals(tab, 0)*1.0 # multiply by 1.0 detach from crate        
+        gg = get_colvals(tab, 1)*1.0
+        bb = get_colvals(tab, 2)*1.0
+        return (rr, gg, bb)
+
+    def _unzip_stuff( filename ):        
+        if 3 != len(filename):
+            raise IndexError("The input tuple must have 3 elements")
+        
+        r = filename[0]
+        g = filename[1]
+        b = filename[2]
+
+        if len(r) != len(g) or len(r) != len(b) or len(g) != len(b):
+            raise IndexError("Must have the same number of red, green, and blue values")
+
+        if any( map( lambda x: x<0.0 or x>1.0, r )):
+            raise IndexError( "All the red values must be between 0 and 1.")
+        if any( map( lambda x: x<0.0 or x>1.0, g )):
+            raise IndexError( "All the green values must be between 0 and 1.")
+        if any( map( lambda x: x<0.0 or x>1.0, b )):
+            raise IndexError( "All the blue values must be between 0 and 1.")
+
+        return (r,g,b)
 
     def _create_hex_codes( rr, gg, bb ):
         """
@@ -89,10 +117,11 @@ def _sample_from_lut( lutfile, num_sample, reverse=False, invert=False, xform=_l
         
         Conver the rgb values into their 6digit hex color code
         """
-        myclr = _try_hard_to_locate(filename)        
-        rr = get_colvals(myclr, 0)*1.0 # multiply by 1.0 detach from crate        
-        gg = get_colvals(myclr, 1)*1.0
-        bb = get_colvals(myclr, 2)*1.0
+
+        if isinstance( filename, basestring ):
+            rr,gg,bb = _try_hard_to_locate(filename)        
+        else:
+            rr,gg,bb = _unzip_stuff(filename)
 
         if reverse:
             rr = rr[::-1]
